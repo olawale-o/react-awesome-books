874ec9ca725d0ce2695d752beaf1c2e317a64638
olawale-o <omoogunolawale@gmail.com>
2021-08-08 00:42:05 +0100

    Implement useReducer in Books.js file

--- src/views/Books.js
+++ src/views/Books.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import BookList from '../components/BookList.js';
 import AddBook from '../components/AddBook.js';
+import bookReducer from '../store/bookReducer.js';
+import { initialState } from '../store/bookState.js';
 
-const Books = () =>{
-    return (
-        <div>
-            <BookList />
-            <AddBook />
-        </div>
-    )
+const Books = () => {
+  const [ state, dispatch ] = useReducer(bookReducer, initialState);
+  const AddBookToStore = ({ title, author }) => {
+    dispatch({ type: 'addBook', payload: {id: 3, title, author } });
+  }
+  return (
+    <div>
+      <BookList books={state.books} />
+        <AddBook AddBookToStore={AddBookToStore} />
+    </div>
+  )
 }
 
 export default Books
