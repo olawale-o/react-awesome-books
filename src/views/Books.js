d9197d5a236a8037db3c4b303fad1da8950f37e9
olawale-o <omoogunolawale@gmail.com>
2021-08-07 19:28:41 +0100

    Import BookList and AddBook in Books.js file

---
+++ src/views/Books.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BookList from '../components/BookList.js';
+import AddBook from '../components/AddBook.js';
+
+const Books = () =>{
+    return (
+        <div>
+            <BookList />
+            <AddBook />
+        </div>
+    )
+}
+
+export default Books
