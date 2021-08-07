b329e2309c862f85a036622b4ff39f295db1bd7a
olawale-o <omoogunolawale@gmail.com>
2021-08-08 00:40:28 +0100

    Implement AddBookToStore function in AddBook.js file

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,8 +1,37 @@
+import { useState } from "react";
+const AddBook = ({ AddBookToStore }) => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
 
-const AddBook = () => {
+  const handleAuthor = (event) => {
+    setAuthor(event.target.value);
+  };
+
+  const handleTitle = (event) => {
+    setTitle(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    AddBookToStore({ title, author })
+    setTitle('');
+    setAuthor('');
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      Add Book
+    <div className="form" id="form">
+      <h2>Add a new book</h2>
+      <form className="add-book-form" id="add-book-form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" name="title" value={title} placeholder="Title" required onChange={handleTitle} />
+          <p id="title-error"></p>
+        </div>
+        <div>
+          <input type="text" name="author" value={author} placeholder="Author" required onChange={handleAuthor} />
+          <p id="author-error"></p>
+        </div>
+        <input type="submit" value="Add" id="add-book-button" />
+      </form>
     </div>
   )
 }
