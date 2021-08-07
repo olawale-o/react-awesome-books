41327771fae07a0b56c5993b41ce03ccbd8cac75
olawale-o <omoogunolawale@gmail.com>
2021-08-08 00:37:59 +0100

    Add bookState file

---
+++ src/store/bookState.js
@@ -0,0 +1,14 @@
+export const initialState = {
+  books: [
+    {
+      id: 1,
+      title: 'kings men',
+      author: 'wale',
+    },
+    {
+      id: 2,
+      title: 'Art of war',
+      author: 'sun tzu',
+    },
+  ]
+};
