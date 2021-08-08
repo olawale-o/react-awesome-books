dc868a7982d24087b797cd83d187197e7bea8148
olawale-o <omoogunolawale@gmail.com>
2021-08-08 21:33:13 +0100

    Wrap BookList and AddBook components with BookContext in Books.js file

--- src/views/Books.js
+++ src/views/Books.js
@@ -1,18 +1,25 @@
 import React, { useReducer } from 'react'
 import BookList from '../components/BookList.js';
 import AddBook from '../components/AddBook.js';
-import bookReducer from '../store/bookReducer.js';
+import { bookReducer } from '../store/bookReducer.js';
 import { initialState } from '../store/bookState.js';
+import { BookContext } from '../contextProvider/bookProvider.js';
 
 const Books = () => {
   const [ state, dispatch ] = useReducer(bookReducer, initialState);
-  const AddBookToStore = ({ title, author }) => {
+
+  const addBookToStore = ({ title, author }) => {
     dispatch({ type: 'addBook', payload: {id: 3, title, author } });
   }
+
+  // Use this when an init function is passed as a params to to useReducer
+  // const emptyBooks = () => dispatch({type: 'emptyBooks', payload: [] });
   return (
     <div>
-      <BookList books={state.books} />
-        <AddBook AddBookToStore={AddBookToStore} />
+      <BookContext.Provider value={[state, dispatch]}>
+        <BookList books={state.books} />
+        <AddBook addBookToStore={addBookToStore} />
+      </BookContext.Provider>
     </div>
   )
 }
