2b50ea8ed1a010c0e41e531c78c7a2fb59fac60e
olawale-o <omoogunolawale@gmail.com>
2021-08-11 20:51:15 +0100

    Add Footer.js file

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <div className="copyright">
+          <p>Copyright</p>
+          <p>All rights reserved <span class="year" id="year"></span></p>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer
