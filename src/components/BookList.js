718d1266d05c786c8bbfb3f17da958f22b4ab8be
olawale-o <omoogunolawale@gmail.com>
2021-08-11 17:58:32 +0100

    Add toggle props in BookList.js file

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,12 @@
 import Book from './Book.js';
 
 
-const BookList = ({ books }) => {
+const BookList = ({ books, toggle }) => {
 
   const bookList = books.map(({ id, title, author }) => 
     <Book key = {id} title = {title} author = { author } /> );
   return (
-    <div className="books">
+    <div className={toggle === 1 ? "books" : "books hide"}>
       <div className="book-heading">
         <h2>All awesome books</h2>
        </div>
