88bc1489ab2bbc70d233ec75db986ad947678c43
olawale-o <omoogunolawale@gmail.com>
2021-08-11 14:15:55 +0100

    Implement useSelector and useDispatch hooks in Books.js file

--- src/views/Books.js
+++ src/views/Books.js
@@ -1,15 +1,22 @@
 import React, { useReducer } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import BookList from '../components/BookList.js';
 import AddBook from '../components/AddBook.js';
 import { bookReducer } from '../store/bookReducer.js';
 import { initialState } from '../store/bookState.js';
 import { BookContext } from '../contextProvider/bookProvider.js';
+import { addBook } from '../reduxStore/actions/action_creators.js';
+import { selectBooks, totalBooks } from '../reduxStore/selectors/booksSelector';
 
 const Books = () => {
   const [ state, dispatch ] = useReducer(bookReducer, initialState);
+  const books = useSelector(selectBooks);
+  const booksLen = useSelector(totalBooks);
+  const dispatchAction = useDispatch();
 
   const addBookToStore = ({ title, author }) => {
-    dispatch({ type: 'addBook', payload: {id: 3, title, author } });
+    // dispatch({ type: 'addBook', payload: {id: 3, title, author } });
+    dispatchAction(addBook({id: booksLen + 1, title, author, }));
   }
 
   // Use this when an init function is passed as a params to to useReducer
@@ -17,7 +24,7 @@ const Books = () => {
   return (
     <div>
       <BookContext.Provider value={[state, dispatch]}>
-        <BookList books={state.books} />
+        <BookList books={books} />
         <AddBook addBookToStore={addBookToStore} />
       </BookContext.Provider>
     </div>
