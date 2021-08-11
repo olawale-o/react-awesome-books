542d6992946157a8c14bb14a54cd0a961858e26f
olawale-o <omoogunolawale@gmail.com>
2021-08-11 14:11:53 +0100

    Add bookState.js file

---
+++ src/reduxStore/books/bookState.js
@@ -0,0 +1,14 @@
+const bookState = [
+  {
+    id: 1,
+    title: 'kings men',
+    author: 'wale',
+  },
+  {
+    id: 2,
+    title: 'Art of war',
+    author: 'sun tzu',
+  },
+];
+
+export default bookState;
