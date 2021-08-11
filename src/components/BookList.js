866dce1ad6ef97c57058a75eb308a5f142f0c81f
olawale-o <omoogunolawale@gmail.com>
2021-08-11 17:36:11 +0100

    Add heading in BookList.js file

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,8 +6,13 @@ const BookList = ({ books }) => {
   const bookList = books.map(({ id, title, author }) => 
     <Book key = {id} title = {title} author = { author } /> );
   return (
-    <div>
-      { bookList }
+    <div className="books">
+      <div className="book-heading">
+        <h2>All awesome books</h2>
+       </div>
+       <ul className="list-of-books">
+        { bookList }
+       </ul>
     </div>
   )
 }
