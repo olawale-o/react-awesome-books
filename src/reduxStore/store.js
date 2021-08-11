9f5792f1061a37fbca214171be1d624080eae9b9
olawale-o <omoogunolawale@gmail.com>
2021-08-11 14:14:07 +0100

    Add store.js file

---
+++ src/reduxStore/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+import rootReducer from "./rootReducer";
+
+const configStore = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
+// use this when you import createStore
+// const store = createStore(bookReducer);
+
+export default configStore;
