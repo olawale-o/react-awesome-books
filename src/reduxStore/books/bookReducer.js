20cdf58c66f20c97d66e73e3ebc37a2f50546daa
olawale-o <omoogunolawale@gmail.com>
2021-08-11 20:42:03 +0100

    Add book/removeBook case in bookReducer.js file

--- src/reduxStore/books/bookReducer.js
+++ src/reduxStore/books/bookReducer.js
@@ -4,7 +4,9 @@ export default function bookReducer(state = appState, action) {
   switch (action.type) {
     case 'book/AddBook':
       return { ...state, books: [ ...state.books, action.payload ]  };
-  
+    case 'book/removeBook':
+      const books = state.books.filter((book) => action.payload !== book.id);
+      return { books, };
     default:
       return state;
   }
