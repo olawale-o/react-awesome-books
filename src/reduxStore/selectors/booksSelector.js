693ab629bfbcab7984856d22871de620358e816c
olawale-o <omoogunolawale@gmail.com>
2021-08-11 14:13:17 +0100

    Add booksSelector.js file

---
+++ src/reduxStore/selectors/booksSelector.js
@@ -0,0 +1,7 @@
+// use this when you not using combineReducer
+// export const booksSelector = state => state.books;
+
+// use this to select the content of your store when you using combineReducer
+export const selectBooks = state => state.books.books;
+export const totalBooks = state => state.books.books.length;
+
