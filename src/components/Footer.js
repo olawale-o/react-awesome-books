ea9a73a39039dbffb3fa2d77a67e0c00be7322f6
olawale-o <omoogunolawale@gmail.com>
2021-08-12 07:18:04 +0100

    Implement year update in Footer.js file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { DateTime } from 'luxon';
 
 function Footer() {
+  const year  = DateTime.now().year;
     return (
       <footer className="footer">
         <div className="copyright">
           <p>Copyright</p>
-          <p>All rights reserved <span class="year" id="year"></span></p>
+          <p>All rights reserved <span class="year" id="year">{year}</span></p>
         </div>
       </footer>
     )
